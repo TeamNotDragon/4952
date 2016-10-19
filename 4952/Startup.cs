4cb9c712bb117f6e32e161575146df3ab7411b56
Jason Burns <admiralmudkip@gmail.com>
2016-10-19 11:14:15 -0700

    initial commit

---
+++ 4952/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4952.Startup))]
+namespace _4952
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
