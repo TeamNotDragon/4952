1923cdf34b87651aa8b5ba3e663490e737adc6b9
Justin <justin.mcle@gmail.com>
2016-11-09 22:30:07 -0800

    Css Cleanup and Changes to Login Screen

--- 4952/Models/UserAccount.cs
+++ 4952/Models/UserAccount.cs
@@ -13,10 +13,12 @@ namespace _4952.Models
 
         [Required(ErrorMessage = "Email is required.")]
         [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid email address")]
+        [Display(Name ="Email")]
         public string email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string password { get; set; }
     }
 }
