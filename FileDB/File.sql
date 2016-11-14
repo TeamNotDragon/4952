181b1791483809bdeac887d3397ae070cab1e562
limbermuffin <limbermuffin@gmail.com>
2016-11-14 08:44:41 -0800

    Added DB project

---
+++ FileDB/File.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[File]
+(
+	[FileID] INT NOT NULL IDENTITY,
+	[userID]          INT             NOT NULL,
+    [data]            VARBINARY (MAX) NOT NULL,
+    [fileName]        NCHAR (255)     NOT NULL,
+    [fileSize]        INT      NOT NULL,
+    [fileDateCreated] DATETIME      NOT NULL,
+	PRIMARY KEY CLUSTERED ([fileID] ASC),
+	CONSTRAINT [userID] FOREIGN KEY ([userID]) REFERENCES [dbo].[User] ([userID])
+)
---
+++ FileDB/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[userID] INT NOT NULL IDENTITY,
+    [email]    NCHAR (255) NOT NULL,
+    [password] NCHAR (255) NOT NULL,
+	PRIMARY KEY CLUSTERED ([userID] ASC)
+)
