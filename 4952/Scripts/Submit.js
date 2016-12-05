5afe305858d650187243b0e8e7868744b53d9995
limbermuffin <limbermuffin@gmail.com>
2016-12-05 11:15:22 -0800

    Stop encryption string clearing after file upload

--- 4952/Scripts/Submit.js
+++ 4952/Scripts/Submit.js
@@ -1,10 +1,19 @@
-﻿function upload() {
+﻿window.onload = function () {
+    var hashField = document.getElementById("hashField");
+    if (sessionStorage.getItem("hashField")) {
+        hashField.value = sessionStorage.getItem("hashField");
+    }
+    hashField.addEventListener("keyup", function () {
+        sessionStorage.setItem("hashField", hashField.value);
+    });
+}
+
+function upload() {
     var reader = new FileReader();
     reader.onload = function (e) {
         var encrypted = CryptoJS.AES.encrypt(e.target.result, document.getElementById("hashField").value);
         var filePath = document.getElementById("choose_file").value;
         var fileName = filePath.substring(filePath.lastIndexOf('\\') + 1);
-
         $.ajax({
             type: 'POST',
             url: '/Home/UploadFile/',
@@ -15,7 +24,6 @@
                 location.reload();
             }
         });
-
     }
     var file = document.getElementById("choose_file").files[0];
     reader.readAsDataURL(file);
@@ -45,10 +53,14 @@ function deleteFile() {
     window.location.href = "/Home/DeleteFile/" + $("input[name='rGroup']:checked").val();
 }
 
+function logout() {
+    sessionStorage.clear();
+    window.location.href = "/";
+}
+
 jQuery(function () {
     $('input[name="rGroup"]').on('change', function () {
         var fileName = $('input[name="rGroup"]:checked').attr("data-name");
-
         if (fileName !== undefined) {
             $("#fileDetails1").text("File name: " + fileName);
             $("#fileDetails2").text("File size: " + $('input[name="rGroup"]:checked').attr("data-size") + " bytes");
